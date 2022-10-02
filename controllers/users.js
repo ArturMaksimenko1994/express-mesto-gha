b6ac351e841ce190c5a9c7afa9ac2cf712f7442b
Arthur Maksimenko <arturmaksimenko784@gmail.com>
2022-10-02 21:38:00 +0300

    ver.31

--- controllers/users.js
+++ controllers/users.js
@@ -55,7 +55,7 @@ const login = (req, res, next) => {
             return next(new ErrorUnauthorization('Неправильные почта или пароль'));
           }
           // вернём токен
-          return res.status(200).cookie('jwt', token, { maxAge: 604800000, httpOnly: true, sameSite: true });
+          res.send({ token });
         })
         .catch(() => {
           next(new ErrorUnauthorization('Введите почту и пароль'));
